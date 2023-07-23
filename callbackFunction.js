1683f0bfbaabc7e2b7fb990e22831f82318b3d9b
Naimur <naimur.r61n@gmail.com>
2023-07-23 14:31:50 +0600

    first commit

---
+++ callbackFunction.js
@@ -0,0 +1,9 @@
+function sum(a, b, callback) {
+   console.log(`${a} and ${b} sum result is ${a + b}`);
+   callback();
+}
+
+
+
+
+sum(40, 59, _ => console.log('I had completed my task.'))
---
+++ function.js
@@ -0,0 +1,12 @@
+function firstAssignment() {
+   console.log("I completed my first assignment.");
+}
+
+setTimeout(firstAssignment, 2000);
+
+
+function sum(a, b) {
+   console.log(a + b);
+}
+
+sum(5, 20)
